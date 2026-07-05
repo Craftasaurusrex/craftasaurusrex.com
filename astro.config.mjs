b1d94301e123ed8e4b94a7f360d22d5dc67f2435
DinoSourcesRex <dino.sources.rex@gmail.com>
2026-07-05 15:57:52 +0200

    chore: add github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://craftasaurus.com',
+  site: 'https://craftasaurusrex.com',
   vite: {
     plugins: [tailwindcss()],
   },
